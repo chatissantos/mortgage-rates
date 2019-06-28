01dd8d8ef45b95556f7d16d4a016cd0d4b90da14
chatis <chatis@becurity.com>
2019-06-28 12:58:32 -0500

    added header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import './styles/main.scss';
+import 'bootstrap';
+
 console.log('Welcome to Mortgage Rates');
+function removeClass() {
+    var el = document.querySelector(".waves");
+    el.classList.remove("more");
+}
+
+
+setTimeout(function(){
+    removeClass();
+}, 7500);
